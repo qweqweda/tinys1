30522e51ea2836a169e7acb99247e6674e5caeab
yangyang <2414779368@qq.com>
2024-03-06 02:56:56 -0800

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,8 +9,8 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-black p-2 p-x-5 text-8 color-white>
-        aparbiswal@icloud.com
+      <div rd-2 bg-white p-2 p-x-5 text-8 color-black>
+        Mirsarohi@icloud.com
       </div>
     </div>
   </div>
