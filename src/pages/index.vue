74842d074ad751c63b7599f63ea6f2fa77ced406
yangyang <2414779368@qq.com>
2024-03-19 23:19:10 -0700

    update

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ defineOptions({
     <img h-full w-full src="../assets/bg.webp" alt="">
 
     <div class="email" absolute bottom-10 w-full flex justify-center>
-      <div rd-2 bg-yellow p-2 p-x-5 text-8 color-black >
+      <div rd-2 bg-lime p-2 p-x-5 text-8 color-black >
         639293382@qq.com
       </div>
     </div>
